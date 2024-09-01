3ab468b5e26f83f124cc88793c3e02009184ca35
Guy Lin <166591803+guychienll-kkday@users.noreply.github.com>
2024-09-01 21:41:12 +0800

    feat: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 import locales from "./src/translation/resource.js";
 import i18nConfig from "./next-i18next.config.js";
+import pwa from "next-pwa"
+
+const withPWA = pwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -25,4 +33,4 @@ const nextConfig = {
   i18n: i18nConfig.i18n,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
