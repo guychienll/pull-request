1ad6da8781468a39b17853ee35fd6005f8206b7d
Guy Lin <166591803+guychienll-kkday@users.noreply.github.com>
2024-10-04 17:09:41 +0800

    feat: add case for no prs need to review

--- src/pages/api/sendUnreadNotification.ts
+++ src/pages/api/sendUnreadNotification.ts
@@ -37,60 +37,93 @@ export default async function handler(
         new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
     );
 
-  const message = {
-    text: "Sprint 需要 review 的 PR",
-    blocks: [
-      {
-        type: "header",
-        text: {
-          type: "plain_text",
-          text: "🚀 Sprint 需要 review 的 PR",
-          emoji: true,
-        },
-      },
-      {
-        type: "divider",
-      },
-      {
-        type: "context",
-        elements: [
-          {
-            type: "mrkdwn",
-            text: `共有 ${filteredPullRequests.length} 個 PR 需要 review`,
-          },
-        ],
-      },
-      ...filteredPullRequests.map((pr) => ({
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `*<${pr.html_url}|${pr.title}>*\n作者: ${pr.user.login} | 創建於: ${new Date(pr.created_at).toLocaleString("zh-TW")}`,
-        },
-        accessory: {
-          type: "button",
+  let message = null;
+
+  if (filteredPullRequests.length > 0) {
+    message = {
+      text: "Sprint 需要 review 的 PR",
+      blocks: [
+        {
+          type: "header",
           text: {
             type: "plain_text",
-            text: "查看 PR 👀",
+            text: "🚀 Sprint 需要 review 的 PR",
             emoji: true,
           },
-          url: pr.html_url,
-          action_id: "button-action",
         },
-      })),
-      {
-        type: "divider",
-      },
-      {
-        type: "context",
-        elements: [
-          {
+        {
+          type: "divider",
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: `共有 ${filteredPullRequests.length} 個 PR 需要 review`,
+            },
+          ],
+        },
+        ...filteredPullRequests.map((pr) => ({
+          type: "section",
+          text: {
             type: "mrkdwn",
-            text: "請及時 review 以上 PR，以確保開發進度 💪",
+            text: `*<${pr.html_url}|${pr.title}>*\n作者: ${pr.user.login} | 創建於: ${new Date(pr.created_at).toLocaleString("zh-TW")}`,
           },
-        ],
-      },
-    ],
-  };
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "查看 PR 👀",
+              emoji: true,
+            },
+            url: pr.html_url,
+            action_id: "button-action",
+          },
+        })),
+        {
+          type: "divider",
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: "請及時 review 以上 PR，以確保開發進度 💪",
+            },
+          ],
+        },
+      ],
+    };
+  } else {
+    message = {
+      text: "🎉 今天沒有需要 review 的 PR，大家辛苦了！",
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "🎉 今天沒有需要 review 的 PR，大家辛苦了！",
+            emoji: true,
+          },
+        },
+        {
+          type: "image",
+          image_url:
+            "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExczZpNjdybXBkb213YmM3b3Y1MjhzNG04emJ2ZXY3ZDdreG8xM3hweiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kyLYXonQYYfwYDIeZl/giphy.gif",
+          alt_text: "gif",
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: "今天是個好日子，讓我們繼續保持這樣的工作效率！💪",
+            },
+          ],
+        },
+      ],
+    };
+  }
 
   await axios.post(slackWebhook as string, message);
 
